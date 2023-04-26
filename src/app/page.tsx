b49ab662ee80200a48b05278b26a058a99d0afa5
rnfkaus8 <kth4350@gmail.com>
2023-04-26 21:32:47 +0900

    add posts page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h4 className="title">Get started</h4>
       <p className="sub-title">by kim tae hyun</p>
+      <Link href="/posts">Posts</Link>
     </div>
   );
 }
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Posts = () => {
+  return (
+    <>
+      <h1>Posts Page</h1>
+      <Link href="/">Back to Home</Link>
+    </>
+  );
+};
+
+export default Posts;
