cf36e71a3415de7893dff2da75a6eff007a3a6bd
rnfkaus8 <kth4350@gmail.com>
2023-04-27 19:20:15 +0900

    add tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h4 className="title">Get started</h4>
-      <p className="sub-title">by kim tae hyun</p>
-      <Link href="/posts">Posts</Link>
-    </div>
-  );
+  return <div className="text-rose-500 text-2xl">Hello Airbnb</div>;
 }
