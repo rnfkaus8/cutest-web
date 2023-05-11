d3f5d1e69ade168b291d608730b0e20da5ce4e75
rnfkaus8 <kth4350@gmail.com>
2023-05-11 20:27:50 +0900

    add UserMenu component

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Container from "@/app/components/Container";
 import Logo from "@/app/components/navbar/Logo";
 import Search from "@/app/components/navbar/Search";
+import UserMenu from "@/app/components/navbar/UserMenu";
 
 const Navbar = () => {
   return (
@@ -10,6 +11,7 @@ const Navbar = () => {
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
             <Search />
+            <UserMenu />
           </div>
         </Container>
       </div>
