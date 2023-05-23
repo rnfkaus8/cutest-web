fb2274f7d708e3eb05ba70c539c9ad008f4a2026
rnfkaus8 <kth4350@gmail.com>
2023-05-23 19:58:54 +0900

    add Modal design

--- src/app/components/modals/Modal.tsx
+++ src/app/components/modals/Modal.tsx
@@ -1,6 +1,10 @@
-"use client";
+'use client';
 
-import React, { useCallback, useEffect, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 
 interface ModalProps {
   isOpen?: boolean;
@@ -61,7 +65,40 @@ const Modal: React.FC<ModalProps> = ({
     return null;
   }
 
-  return <div></div>;
+  return (
+    <>
+      <div
+        className="
+          justify-center
+          items-center
+          flex
+          overflow-x-hidden
+          overflow-y-auto
+          fixed
+          inset-0
+          z-50
+          outline-none
+          focus:outline-none
+          bg-neutral-800/70
+        "
+      >
+        <div
+          className="
+            relative
+            w-full
+            md:w-4/6
+            lg:w-3/6
+            xl:w-2/5
+            my-6
+            mx-auto
+            h-full
+            lg:h-auto
+            md:h-auto
+          "
+        ></div>
+      </div>
+    </>
+  );
 };
 
 export default Modal;
